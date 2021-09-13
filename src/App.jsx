873ab8473e838d7c95d8784219aad98b28a8d009
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2021-09-13 11:58:57 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Template from "./components/template";
+import Meme from "./components/meme";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const Memer = () => {
+  const [templates, setTemplate] = useState([]); //for the main page
+  const [meme, setMeme] = useState(false);
+  // for the chosen meme
+
+  const fetchTemplate = async () => {
+    const response = await fetch("https://api.imgflip.com/get_memes");
+    const data = await response.json();
+    // console.log(data.data);
+    // console.log(data.data.memes);
+    const memeTemplateJSON = data.data.memes; //meme template image obtained from the API .
+    setTemplate(memeTemplateJSON);
+  };
+  useEffect(() => {
+    fetchTemplate();
+    // ();
+  }, []);
+
+  return (
+    <div>
+      <h1>Meme Generator</h1>
+      {!meme ? (
+        <Template templates={templates} setMeme={setMeme} />
+      ) : (
+        <Meme meme={meme} templates={templates} setMeme={setMeme} />
+      )}
+    </div>
+  );
+};
+export default Memer;
